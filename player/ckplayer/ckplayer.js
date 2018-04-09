4d0aa7deff84254dac94886c5ceef1a3c8aa2256
cmpyya <38219400+cmpyya@users.noreply.github.com>
2018-04-10 01:27:16 +0800

    Update ckplayer.js

--- player/ckplayer/ckplayer.js
+++ player/ckplayer/ckplayer.js
@@ -1,4 +1,4 @@
-﻿function ckplayerConfig() {
+function ckplayerConfig() {
 	return {
 		flashvars: {}, //用来补充flashvars里的对象
 		languagePath: '', //语言包文件地址
@@ -173,15 +173,6 @@
 			['008', 'Ajax error'],
 			['009', 'Ajax object format error'],
 			['010', 'Ajax.status:[error]']
-		],
-		//全局变量/变量类型：Array/功能：HTML5变速播放的值数组/如果不需要可以设置成null
-		playbackRateArr: [
-			[0.5, '0.5倍'],
-			[1, '正常'],
-			[1.25, '1.25倍'],
-			[1.5, '1.5倍'],
-			[2, '2倍速'],
-			[4, '4倍速']
 		],
 		//全局变量/变量类型：String/功能：定义logo
 		logo: '',
